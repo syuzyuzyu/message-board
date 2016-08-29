23430075d4832b356074cbfa4453105424c0ff7a
ana <syuzyuzyu@gmail.com>
2016-08-29 14:10:47 +0000

    finish challenge2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,7 +40,7 @@ class MessagesController < ApplicationController
   
   
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   def set_message
     @message = Message.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827121223) do
+ActiveRecord::Schema.define(version: 20160829134001) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
